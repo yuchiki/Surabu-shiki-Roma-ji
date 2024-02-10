ca9b6a5e8247b938ec940fc14c238fc07693d7f0
yuchiki <14884013+yuchiki@users.noreply.github.com>
2024-02-10 23:38:10 +0900

    ci (#1)

--- makefile
+++ makefile
@@ -1,3 +1,14 @@
-.PHONY: test
+.PHONY: check-all
+check-all: lint typecheck
+
+.PHONY: lint
+lint:
+	poetry run ruff .
+
+.PHONY: typecheck
+typecheck:
+	poetry run mypy .
+
+.PHONY: run
 test:
-	poetry run python Surabu-shiki-Roma-ji/main.py
+	poetry run python suraburomaji/main.py
--- suraburomaji/main.py
+++ suraburomaji/main.py
@@ -1,9 +1,11 @@
-import MeCab
+"""MeCabを使って分かち書きを行うサンプルコードです."""
 
-wakati: int = MeCab.Tagger("-Owakati")
-text = "あらゆる現実を自分の方に捻じ曲げたのだ。"
-text2 = "hoge"
-data = wakati.parse(text)
+import MeCab  # type: ignore[import-untyped]
 
-print(text)
-print(data)
+if __name__ == "__main__":
+    wakati = MeCab.Tagger("-Owakati")
+    text = "あらゆる現実を自分の方に捻じ曲げたのだ。"
+    data = wakati.parse(text)
+
+    print(text)  # noqa: T201
+    print(data)  # noqa: T201
